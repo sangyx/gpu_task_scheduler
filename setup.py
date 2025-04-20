e7adb69348b3cf7ad7cf93995e68cdee2f112cb7
sangyx <sangyunxin@gmail.com>
2025-04-20 18:27:48 +0000

    update lib

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="gpu_task_scheduler",
     version="0.1.0",
-    description="A python lib to schedule tasks on available GPUs.",
+    description="A python lib to schedule deep learning tasks on available GPUs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="sangyx",
     packages=find_packages(),
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.6",
 )
